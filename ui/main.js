088f483722f3bf11633ed2af3c0c1e24dd7c5dd0
arpantopno <103114012@nitt.edu>
2017-08-09 19:23:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+
+document.getElementById('main-text').innerHTML = 'Lol!';
+
+var img = document.getElement.Id('madi');
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+};
