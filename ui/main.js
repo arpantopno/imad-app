7d83281abe01a8021a173159bcb0fed1855ff438
arpantopno <103114012@nitt.edu>
2017-08-09 23:41:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var button = document.getElementById('counter');
 button.onclick = function () {
     
     var request = new XMLHttpRequest();
-    
+    console.log('a');
     request.onreadystatechange = function () {
         if(request.readyState === XMLHttpRequest.DONE) {
             if(request.status === 200) {
