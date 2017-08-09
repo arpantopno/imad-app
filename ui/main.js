3e6c4694fd0009a0e6b06a54f8796347c6fa8df2
arpantopno <103114012@nitt.edu>
2017-08-10 01:09:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,10 +19,11 @@ button.onclick = function () {
     request.send(null);
 };
 
-var name = document.getElementById('name').value;
+
 var submit = document.getElementById('submit');
 submit.onclick = function() {
      
+    var name = document.getElementById('name').value;
     var request = new XMLHttpRequest();
     
     request.onreadystatechange = function () {
@@ -30,6 +31,10 @@ submit.onclick = function() {
             if(request.status === 200) {
                 var names = request.responseText;
                 names = JSON.parse(names);
+                var list = '';
+                for(var i = 0; i < names.length; i++){
+                    lins += '<li>'+namea[i]+'</li>;'
+                }
             }
         }
     };
