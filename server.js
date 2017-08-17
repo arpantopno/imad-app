370a277fcf5d1da47452b43c88e518b3e1682732
arpantopno <103114012@nitt.edu>
2017-08-17 14:54:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,38 +5,6 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-function createTemplate(data) {
-    var title = data.title;
-    var heading = data.heading;
-    var date = data.date;
-    var content = data.content;
-    var htmlTemplate = `
-    <html>
-        <head>
-            <title>${title}</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link href="/ui/style.css" rel="stylesheet" />
-        </head>
-        <body>
-            <div class="container">
-                <div>
-                    <a href="/">Home</a>
-                </div>
-                <hr/>
-                <h3>${heading}</h3>
-                <div>
-                    ${date}
-                </div>
-                <div>
-                    ${content}
-                </div>
-            </div>
-        </body>
-    </html>
-    `;
-    return htmlTemplate;
-}
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -52,12 +20,6 @@ app.get('/submit-name', function(req, res){
     var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
-});
-
-
-app.get('/:articleName', function(req, res){
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
