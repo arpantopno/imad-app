37c39a3016579e219ef41d529aba48f9c658b64e
arpantopno <103114012@nitt.edu>
2017-08-17 20:07:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var pool = require('pg').pool;
 var app = express();
 app.use(morgan('combined'));
 
+var config = {
+    user: 'arpansharon',
+    database: 'arpansharon',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
+
+var pool = new Pool(config);
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/test-db', function (req, res){
+  pool.query('SELECT * FROM test', function(err, result){
+      if(err) {
+          res.send(500).send(err).toString();
+      } else {
+          res.send(JSON.stringify(result));
+      }
+  });
 });
 
 var counter = 0;
