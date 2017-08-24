cf958d741f8b325e1086071c4470b216d473894a
arpantopno <103114012@nitt.edu>
2017-08-24 22:26:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var app = express();
+var crypto = require('crypto');
 app.use(morgan('combined'));
 
 var config = {
@@ -90,6 +91,16 @@ app.get('/submit-name', function(req, res){
     var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
+});
+
+function hash (input, salt) {
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function (req, res) {
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
 });
 
 app.get('/ui/style.css', function (req, res) {
