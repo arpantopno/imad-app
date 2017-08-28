0dcd51ce2176dbfc30f2ea4c1850a77e35b7b06e
arpantopno <103114012@nitt.edu>
2017-08-28 10:33:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,16 +57,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/test-db', function (req, res){
-  pool.query('SELECT * FROM test', function(err, result){
-      if(err) {
-          res.send(500).send(err.toString());
-      } else {
-          res.send(JSON.stringify(result));
-      }
-  });
-});
-
 app.get('/articles/:articleName', function(req, res){
     pool.query("SELECT * FROM article WHERE title  = $1", [req.params.articleName], function(err, result){
        if(err) {
@@ -82,28 +72,10 @@ app.get('/articles/:articleName', function(req, res){
     });
 });
 
-var counter = 0;
-app.get('/counter', function (req, res){
-    counter = counter + 1;
-    res.send(counter.toString());
-});
-
-var names = [];
-app.get('/submit-name', function(req, res){
-    var name = req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
-
 function hash (input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return ['pbkdf2', "10000", salt, hashed.toString('hex')].join('$');
 }
-
-app.get('/hash/:input', function (req, res) {
-   var hashedString = hash(req.params.input, 'this-is-some-random-string');
-   res.send(hashedString);
-});
 
 app.post('/create-user', function (req, res) {
     var username = req.body.username;
